d7c39e82ef1e7a0c56ba2684307d263bf764f509
Ayaka Kawabata <ypqmw923@yahoo.co.jp>
2017-07-09 11:18:55 +0000

    xadd column

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク新規作成ページ</h1>
+    
+    @include('commons.error_messages')
+
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content') !!}
+        
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+
+
+@endsection
